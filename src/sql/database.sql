d29c48bdfa840fe484652a04b50bbce698608526
Candreloss <carlosdavidparadasmendoza@gmail.com>
2026-02-03 18:22:50 -0400

    REESTRUCTURACION

--- src/sql/database.sql
+++
@@ -1,5 +0,0 @@
-USE oi_modulo_db;
-
-insert into registro(nombre, appellido, cedula, correo, telefono, hora, fecha) values ("NOMBRE", "APELLIDO", "CEDULA", "CORREO", "TELEFONO", "12:00", "2024-06-10");
-
-select * from registro;
